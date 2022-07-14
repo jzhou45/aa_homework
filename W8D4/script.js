c61d680163fcb282c4f7b976886256a5c7506418
jzhou45 <jonathanzhou77@gmail.com>
2022-07-13 20:40:23 -0400

    added a button and textbox and another set timeout function alerting textbox message

--- W8D4/script.js
+++ W8D4/script.js
@@ -1,1 +1,6 @@
 const simpleTimeout = window.setTimeout(alert('HAMMERTIME!'), 5000);
+
+function hammerTime(time){
+    time = document.getElementById("timeId").value;
+    window.setTimeout(alert(time + " is hamemrtime!"));
+};
