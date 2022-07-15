82b9515f5b9b65044edf9136d48c3f563d05908e
jzhou45 <jonathanzhou77@gmail.com>
2022-07-14 22:08:53 -0400

    setted up canvas element

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    let canvasElement = document.getElementById('canvas');
+    canvasElement.height = 500;
+    canvasElement.width = 500;
+    let ctx = canvasElement.getContext('2d');
 });
