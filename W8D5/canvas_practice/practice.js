b684dcbb6525c397f61ab8d09a689c2452e0eb3d
jzhou45 <jonathanzhou77@gmail.com>
2022-07-14 22:17:13 -0400

    fixed typos

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let canvasElement = document.getElementById('canvas');
+    const canvasElement = document.getElementById("mycanvas");
     canvasElement.height = 500;
     canvasElement.width = 500;
-    let ctx = canvasElement.getContext('2d');
+    const ctx = canvasElement.getContext("2d");
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 250, 500);
+
+
 });
