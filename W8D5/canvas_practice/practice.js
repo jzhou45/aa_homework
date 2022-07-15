19cb1ee0a8b7704cf4405ddc8e32e366be6ceb76
jzhou45 <jonathanzhou77@gmail.com>
2022-07-14 22:22:36 -0400

    drew a circle

--- W8D5/canvas_practice/practice.js
+++ W8D5/canvas_practice/practice.js
@@ -7,5 +7,11 @@ document.addEventListener("DOMContentLoaded", function(){
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, 250, 500);
 
+    ctx.beginPath();
+    ctx.arc(100, 200, 25, 90, 270);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
 
+    ctx.fillStyle = "white";
+    ctx.fill();
 });
